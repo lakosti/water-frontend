244bad64b2e6fb637ee85c89783087141f79ea09
Yaroslava <yaroslava.hovenko@gmail.com>
2024-08-03 14:24:08 +0300

    add main logic with sign up form

--- src/components/SignInForm/SignInForm.jsx
+++ src/components/SignInForm/SignInForm.jsx
@@ -1,27 +1,44 @@
 import { Field, Formik, Form } from "formik";
+import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
+import * as Yup from "yup";
+import { selectIsLoading } from "../../redux/auth/selectors.js";
+import { useState } from "react";
 
 const initialValue = {
   email: "",
   password: "",
 };
 
-const handleSubmit = () => {
-  // dispatch(logIn(values))
-  //   .unwrap()
-  //   .then(() => {
-  //     toast.success("Successfully logged in");
-  //   })
-  //   .catch(() => {
-  //     toast.error("Incorrect email or password");
-  //   });
-  // actions.resetForm();
-};
+const validationSchemas = Yup.object({
+  email: Yup.string().email("Invalid email address").required("Required"),
+  password: Yup.string().min(6, "Password must be at least 6 characters").required("Required"),
+});
 
 const SignInForm = () => {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSubmit = () => {
+    // dispatch(logIn(values))
+    //   .unwrap()
+    //   .then(() => {
+    //     toast.success("Successfully logged in");
+    //   })
+    //   .catch(() => {
+    //     toast.error("Incorrect email or password");
+    //   });
+    // actions.resetForm();
+  };
   return (
     <>
-      <Formik initialValues={initialValue} onSubmit={handleSubmit}>
+      <Formik
+        initialValues={initialValue}
+        validationSchema={validationSchemas}
+        onSubmit={handleSubmit}
+      >
         <Form>
           <label>
             <span>Email</span>
--- src/components/SignUpForm/SignUpForm.jsx
+++ src/components/SignUpForm/SignUpForm.jsx
@@ -3,17 +3,19 @@ import { useDispatch, useSelector } from "react-redux";
 import { NavLink } from "react-router-dom";
 import { selectIsLoading } from "../../redux/auth/selectors.js";
 import { signUp } from "../../redux/auth/operations.js";
-import Loader from "../Loader/Loader.jsx";
+// import Loader from "../Loader/Loader.jsx";
 import * as Yup from "yup";
-// import { useState } from "react";
+import { useState } from "react";
 
 const initialValue = {
+  name: "",
   email: "",
   password: "",
   repeatPassword: "",
 };
 
 const validationSchemas = Yup.object({
+  name: Yup.string().min(2, "Name must be at least 6 characters").required("Required"),
   email: Yup.string().email("Invalid email address").required("Required"),
   password: Yup.string().min(6, "Password must be at least 6 characters").required("Required"),
   repeatPassword: Yup.string()
@@ -22,11 +24,18 @@ const validationSchemas = Yup.object({
 });
 
 const SignUpForm = () => {
-  // const [showPassword, setShowPassword] = useState(false);
-  // const [showRepeatPassword, setShowRepeatPassword] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [showRepeatPassword, setShowRepeatPassword] = useState(false);
 
   const dispatch = useDispatch();
   const isLoading = useSelector(selectIsLoading);
+
+  const handelClickPassword = () => {
+    setShowPassword((prevState) => !prevState);
+  };
+  const handelClickRepeatPassword = () => {
+    setShowRepeatPassword((prevState) => !prevState);
+  };
 
   const handleSubmit = (values, actions) => {
     delete values.repeatPassword;
@@ -46,28 +55,60 @@ const SignUpForm = () => {
         validationSchema={validationSchemas}
         onSubmit={handleSubmit}
       >
-        <Form>
-          <label>
-            <span>Email</span>
-            <Field type="email" placeholder="Enter your email" name="email" />
-            <ErrorMessage name="email" component="div" />
-          </label>
-          <label>
-            <span>Password</span>
-            <Field type="password" placeholder="Enter your password" name="password" />
-            <ErrorMessage name="password" component="div" />
-          </label>
-          <label>
-            <span>Repeat password</span>
-            <Field type="password" placeholder="Repeat password" name="repeatPassword" />
-            <ErrorMessage name="repeatPassword" component="div" />
-          </label>
-          <button type="submit">{isLoading ? "Loading..." : "Sign Up"}</button>
-          <div>
-            <p>Already have account?</p>
-            <NavLink to="/signin">Sign In</NavLink>
-          </div>
-        </Form>
+        {({ isValid, dirty }) => (
+          <Form>
+            <label>
+              <span>Name</span>
+              <Field type="text" placeholder="Enter your name" name="name" />
+              <ErrorMessage name="name" component="div" />
+            </label>
+            <label>
+              <span>Email</span>
+              <Field type="email" placeholder="Enter your email" name="email" />
+              <ErrorMessage name="email" component="div" />
+            </label>
+            <label>
+              <span>Password</span>
+              <Field
+                type={showPassword ? "text" : "password"}
+                placeholder="Enter your password"
+                name="password"
+              />
+              <ErrorMessage name="password" component="div" />
+              <button type="button" onClick={handelClickPassword}>
+                {showPassword ? "Hide" : "Show"}
+              </button>
+              {/* {showPassword ? (
+              <button type="button" onClick={handelClickPassword}>
+                Hide
+              </button>
+            ) : (
+              <button type="button" onClick={handelClickPassword}>
+                Show
+              </button>
+            )} */}
+            </label>
+            <label>
+              <span>Repeat password</span>
+              <Field
+                type={showRepeatPassword ? "text" : "password"}
+                placeholder="Repeat password"
+                name="repeatPassword"
+              />
+              <ErrorMessage name="repeatPassword" component="div" />
+              <button type="button" onClick={handelClickRepeatPassword}>
+                {showRepeatPassword ? "Hide" : "Show"}
+              </button>
+            </label>
+            <button disabled={!(isValid && dirty)} type="submit">
+              {isLoading ? "Loading..." : "Sign Up"}
+            </button>
+            <div>
+              <p>Already have account?</p>
+              <NavLink to="/signin">Sign In</NavLink>
+            </div>
+          </Form>
+        )}
       </Formik>
     </>
   );
